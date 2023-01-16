55377b670ec285c07bb4cc056aa93722345405d6
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-01-16 08:44:02 +0100

    Tidy up

--- jetblack_order_book/order_book.py
+++ jetblack_order_book/order_book.py
@@ -57,28 +57,32 @@ class OrderBook:
             price: Decimal,
             size: int
     ) -> Tuple[int, List[Fill]]:
-        aggregate_orders = (
-            self.bids if side == Side.BUY
-            else self.offers
-        )
-
+        # Make the limit order.
         order = LimitOrder(self._next_order_id, side, price, size)
         self.orders[order.order_id] = order
         self._next_order_id += 1
 
+        # Get the orders for the side.
+        aggregate_orders_for_side = (
+            self.bids if side == Side.BUY
+            else self.offers
+        )
+
+        # Find where the order should go.
         index = index_of(
-            aggregate_orders,
+            aggregate_orders_for_side,
             lambda x: x.price >= price
         )
         if index == -1:
             # Add new highest price level.
-            aggregate_orders.append(AggregateOrder(order))
-        elif aggregate_orders[index].price == order.price:
-            # Aggregate to an existing price level.
-            aggregate_orders[index].append(order)
+            aggregate_orders_for_side.append(AggregateOrder(order))
+        elif aggregate_orders_for_side[index].price == order.price:
+            # Add the order to an existing price level. Adding to the end
+            # means newer orders are executed first (time weighted).
+            aggregate_orders_for_side[index].append(order)
         else:
             # Insert a new lowest price level
-            aggregate_orders.insert(index, AggregateOrder(order))
+            aggregate_orders_for_side.insert(index, AggregateOrder(order))
 
         return order.order_id, self._match(order.order_id)
 
@@ -89,14 +93,23 @@ class OrderBook:
                 self.offers and
                 self.bids[-1].price >= self.offers[0].price
         ):
+            # The best bid is the highest price (at the end), while the best
+            # offer is the lowest price (at the start).
             best_bids, best_offers = self.bids[-1], self.offers[0]
             while best_bids and best_offers:
+                # The aggregate orders are ordered by time, so the first order
+                # takes precedence.
                 bid, offer = best_bids.popleft(), best_offers.popleft()
+
+                # The price is that of the newest order in case of a cross;
+                # where the newest order price exceeds (rather than matched)
+                # the best opposing price.
                 trade_size = min(bid.size, offer.size)
                 trade_price = (
                     bid.price if bid.order_id == aggressor_order_id
                     else offer.price
                 )
+
                 fills.append(
                     Fill(
                         bid.order_id,
@@ -104,19 +117,26 @@ class OrderBook:
                         trade_price,
                         trade_size)
                 )
+
+                # Decrement the orders by the trade size, then check if the
+                # orders have been completely executed.
                 bid.size -= trade_size
                 offer.size -= trade_size
 
                 if bid.size == 0:
                     del self.orders[bid.order_id]
                 else:
+                    # As execution was incomplete the remainder is put back.
                     best_bids.appendleft(bid)
 
                 if offer.size == 0:
                     del self.orders[offer.order_id]
                 else:
+                    # As execution was incomplete the remainder is put back.
                     best_offers.appendleft(offer)
 
+            # if all orders have been executed at this price level remove the
+            # price level.
             if not best_bids:
                 del self.bids[-1]
             if not best_offers:
