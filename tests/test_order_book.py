ffb30e9d974cfb99d65c1ac6a28a15e90b1754a6
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-01-17 07:55:05 +0100

    Tidy up

--- tests/test_order_book.py
+++ tests/test_order_book.py
@@ -74,23 +74,47 @@ def test_order_book_multiple_fills():
     assert str(order_book) == ' : 10.5x5'
 
 
-def test_order_book_amend_size():
+def test_order_cross():
+    """
+    A cross occurs when a sell/buy order is placed at a price below/above
+    the best bid/offer. In this case the price of the sell/buy is used, rather
+    than the matched price.
+    """
     order_book = OrderBook()
 
-    assert str(order_book) == ' : '
+    assert str(order_book) == ' : ', "the order book should be empty"
 
-    # Add two buy orders at the same price
+    buy1, _ = order_book.add_limit_order(Side.BUY, Decimal('10.5'), 5)
+    buy2, _ = order_book.add_limit_order(Side.BUY, Decimal('11.0'), 10)
+    sell1, fills = order_book.add_limit_order(Side.SELL, Decimal('10.0'), 15)
+
+    assert fills == [
+        Fill(buy2, sell1, Decimal('10.0'), 10),
+        Fill(buy1, sell1, Decimal('10.0'), 5),
+    ], "fills should have price of the sell"
+    assert str(order_book) == ' : ', "the order book should be empty"
+
+
+def test_order_book_amend_size():
+    """
+    The size of a limit order can be changed.
+    """
+
+    order_book = OrderBook()
+
+    assert str(order_book) == ' : ', "the order book should be empty"
+
     _, _ = order_book.add_limit_order(Side.BUY, Decimal('10.5'), 10)
     sell2, _ = order_book.add_limit_order(Side.SELL, Decimal('10.6'), 10)
 
     assert str(
         order_book
-    ) == '10.5x10 : 10.6x10'
+    ) == '10.5x10 : 10.6x10', "the order book should represent the orders"
 
     order_book.amend_limit_order(sell2, 5)
     assert str(
         order_book
-    ) == '10.5x10 : 10.6x5'
+    ) == '10.5x10 : 10.6x5', "the order book should show the change in size"
 
 
 def test_order_book_cancel_order():
