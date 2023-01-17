4a1b6e2f1a229d8748a20c775e9e0c1b83d607cc
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-01-17 11:25:49 +0100

    Added more exports

--- jetblack_order_book/__init__.py
+++ jetblack_order_book/__init__.py
@@ -1,14 +1,21 @@
 """jetblack-order-book"""
 
+from .aggregate_order import AggregateOrder
+from .aggregate_order_side import AggregateOrderSide
 from .exchange_order_book import ExchangeOrderBook
+from .fill import Fill
 from .limit_order import LimitOrder
-from .order_book import OrderBook, Fill
+from .order_book import OrderBook
+from .order_repo import OrderRepo
 from .order_types import Side
 
 __all__ = [
+    'AggregateOrder',
+    'AggregateOrderSide',
     'ExchangeOrderBook',
     'Fill',
     'LimitOrder',
     'OrderBook',
+    'OrderRepo',
     'Side'
 ]
