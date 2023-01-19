96de6f87ee97bfaa835cb67d448919bae4950e57
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-01-19 08:56:07 +0100

    Added another test

--- tests/test_order_book.py
+++ tests/test_order_book.py
@@ -388,47 +388,107 @@ def test_fill_or_kill_cancel_sell_order():
     assert cancels == [sell_id1, sell_id2], "should cancel in order"
 
 
-def test_immediate_or_cancel():
-    """Test for order style immediate or cancel"""
+def test_immediate_or_cancel_buys():
+    """Test buys for order style immediate or cancel"""
 
     order_book = OrderBook()
 
-    buy_id1, _, _ = order_book.add_limit_order(
+    buy_id1, fills, cancels = order_book.add_limit_order(
         Side.BUY,
         Decimal('10'),
         10,
         Style.IMMEDIATE_OR_CANCEL
     )
+    assert buy_id1 is not None, "should generate order"
+    assert not fills, "should not generate fills"
+    assert not cancels, "should not generate cancels"
+
     buy_id2, _, cancels = order_book.add_limit_order(
         Side.BUY,
         Decimal('10'),
         10,
         Style.IMMEDIATE_OR_CANCEL
     )
-    assert not cancels, "can have many IOC at same price"
+    assert buy_id2 is not None, "should generate order"
+    assert not fills, "should not generate fills"
+    assert not cancels, "should not generate cancels"
 
-    buy_id3, _, cancels = order_book.add_limit_order(
+    buy_id3, fills, cancels = order_book.add_limit_order(
         Side.BUY,
         Decimal('11'),
         10,
         Style.IMMEDIATE_OR_CANCEL
     )
     assert buy_id3 is not None, "should generate buy order"
+    assert not fills, "should not generate fills"
     assert cancels == [
         buy_id1,
         buy_id2
     ], "higher priced buy should cancel previous buys"
 
-    sell_id1, fills, cancels = order_book.add_limit_order(
+    sell_id, fills, cancels = order_book.add_limit_order(
         Side.SELL,
         Decimal('11'),
         5,
         Style.IMMEDIATE_OR_CANCEL
     )
-    assert sell_id1 is not None, "should generate sell order"
+    assert sell_id is not None, "should generate sell order"
     assert fills == [
-        Fill(buy_id3, sell_id1, Decimal('11'), 5)
+        Fill(buy_id3, sell_id, Decimal('11'), 5)
     ]
     assert cancels == [buy_id3], "should cancel the partially unfilled buy"
+
+    assert str(order_book) == ' : ', "the order book should be empty"
+
+
+def test_immediate_or_cancel_sells():
+    """Test sells for order style immediate or cancel"""
+
+    order_book = OrderBook()
+
+    sell_id1, fills, cancels = order_book.add_limit_order(
+        Side.SELL,
+        Decimal('11'),
+        10,
+        Style.IMMEDIATE_OR_CANCEL
+    )
+    assert sell_id1 is not None, "should generate order"
+    assert not fills, "should not generate fills"
+    assert not cancels, "should not generate cancels"
+
+    sell_id2, _, cancels = order_book.add_limit_order(
+        Side.SELL,
+        Decimal('11'),
+        10,
+        Style.IMMEDIATE_OR_CANCEL
+    )
+    assert sell_id2 is not None, "should generate order"
+    assert not fills, "should not generate fills"
+    assert not cancels, "should not generate cancels"
+
+    sell_id3, fills, cancels = order_book.add_limit_order(
+        Side.SELL,
+        Decimal('10'),
+        10,
+        Style.IMMEDIATE_OR_CANCEL
+    )
+    assert sell_id3 is not None, "should generate buy order"
+    assert not fills, "should not generate fills"
+    assert cancels == [
+        sell_id1,
+        sell_id2
+    ], "lower priced sell should cancel previous sells"
+
+    buy_id, fills, cancels = order_book.add_limit_order(
+        Side.BUY,
+        Decimal('10'),
+        5,
+        Style.IMMEDIATE_OR_CANCEL
+    )
+    assert buy_id is not None, "should generate order"
+    assert fills == [
+        Fill(buy_id, sell_id3, Decimal('10'), 5)
+    ]
+    assert cancels == [sell_id3], "should cancel the partially unfilled sell"
 
     assert str(order_book) == ' : ', "the order book should be empty"
